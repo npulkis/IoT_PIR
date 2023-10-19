8bcb2e5886cfc03ba08ffd0a6a3d7c9cdb3c26b9
Nathan Pulkis <d00239038@student.dkit.ie>
2023-10-19 10:39:26 +0100

    Boilerplate

---
+++ flash.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(8,GPIO.OUT)
+GPIO.setwarnings(False)
+
+def flash_led(repeat):
+    for i in range(0, repeat):
+        GPIO.output(11, True)
+        GPIO.output(8, False)
+        time.sleep(0.5)
+        GPIO.output(11, False)
+        GPIO.output(8, True)
+        time.sleep(0.5)
+    GPIO.cleanup()
+    
+
+if __name__ == '__main__':
+    flash_led(10)
+    
